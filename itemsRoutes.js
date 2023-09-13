f9fb02187e4fc8a85f613ddec1ab10022a1ab785
Christopher Alley <calleyviola@gmail.com>
2023-09-13 19:54:53 -0400

    Add routes for patch and delete

--- itemsRoutes.js
+++ itemsRoutes.js
@@ -1,26 +1,66 @@
 const express = require("express");
 const items = require("./fakeDb");
+const { NotFoundError } = require("./expressError")
 const router = new express.Router();
 
 /** Return list of shopping items. */
-router.get("/", function(req, res) {
-  return res.json({items});
-})
+router.get("/", function (req, res) {
+  return res.json(items);
+});
 
 /** Accepts JSON body, adds item, and returns it. */
-router.post("/", function(req, res) {
+router.post("/", function (req, res) {
   const name = req.body.name;
   const price = req.body.price;
-  return res.json({added: { name, price }});
-})
+  const newItem = { name, price };
+  items.items.push(newItem);
+  return res.json({ added: newItem });
+});
 
 /** Accepts JSON body, adds item, and returns it. */
-router.get("/:name", function(req, res) {
+router.get("/:name", function (req, res) {
   const itemName = req.params.name;
 
-  for (const item of items) {
+  for (const item of items.items) {
     if (item.name === itemName) {
       return res.json(item);
     }
   }
-})
+
+  throw new NotFoundError(`${itemName} was not found`)
+});
+
+/** Accepts JSON body, modifies item, and returns it. */
+router.patch("/:name", function (req, res) {
+  const itemName = req.params.name;
+
+  const newName = req.body.name;
+  const newPrice = req.body.price;
+
+  for (const item of items.items) {
+    if (item.name === itemName) {
+      item.name = newName;
+      item.price = newPrice;
+      return res.json({updated: item});
+    }
+  }
+
+  throw new NotFoundError(`${itemName} was not found`)
+});
+
+/** Deletes items, returns JSON confirming deletion */
+router.delete("/:name", function (req, res) {
+  const itemName = req.params.name;
+
+  for (let i=0; i<items.items.length; i++) {
+    if (items.items[i].name === itemName) {
+      items.items.splice(i, 1)
+
+      return res.json({message: "Deleted"});
+    }
+  }
+
+  throw new NotFoundError(`${itemName} was not found`)
+});
+
+module.exports = router;
